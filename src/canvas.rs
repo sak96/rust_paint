bc1a99467493ec79aca05f307f7efa0eee80d3f8
sak96 <26397224+sak96@users.noreply.github.com>
2022-05-15 12:56:55 +0530

    Move rendering logic to canvas

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,16 +1,22 @@
+use wgpu::{
+    util::{BufferInitDescriptor, DeviceExt},
+    BufferUsages, Color, CommandEncoderDescriptor, LoadOp, Operations, RenderPassColorAttachment,
+    RenderPassDescriptor, ShaderStages,
+};
 use winit::dpi::PhysicalSize;
+
+use crate::{brush::Point, colorwheel::ColorWheel};
 
 pub struct Canvas {
     window_size: PhysicalSize<u32>,
+    strokes: Vec<Point>,
+    colorwheel: ColorWheel,
 }
 
 impl Canvas {
-    pub fn new(window_size: PhysicalSize<u32>) -> Self {
-        Self { window_size }
-    }
-
-    pub fn resize_window(&mut self, window_size: PhysicalSize<u32>) {
-        self.window_size = window_size
+    pub fn add_stroke(&mut self, start: Point, end: Point) {
+        self.strokes.push(start);
+        self.strokes.push(end);
     }
 
     #[must_use = "converted position must be used"]
@@ -18,5 +24,72 @@ impl Canvas {
         let width = self.window_size.width as f32;
         let height = self.window_size.height as f32;
         [pos[0] / width - 0.5, -pos[1] / height + 0.5]
+    }
+
+    pub fn new(window_size: PhysicalSize<u32>) -> Self {
+        Self {
+            window_size,
+            strokes: vec![],
+            colorwheel: ColorWheel::default(),
+        }
+    }
+
+    pub fn redraw_canvas(
+        &mut self,
+        device: &wgpu::Device,
+        surface: &wgpu::Surface,
+        render_pipeline: &wgpu::RenderPipeline,
+        queue: &wgpu::Queue,
+    ) {
+        let vertex_buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: None,
+            contents: bytemuck::cast_slice(&self.strokes),
+            usage: BufferUsages::VERTEX,
+        });
+        let output_texture = surface
+            .get_current_texture()
+            .expect("failed to get texture for rendering");
+        let view = output_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor {
+            label: Some("paint encoder"),
+        });
+        {
+            let mut rpass = encoder.begin_render_pass(&RenderPassDescriptor {
+                label: None,
+                color_attachments: &[RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: Operations {
+                        load: LoadOp::Clear(Color::WHITE),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+            rpass.set_pipeline(render_pipeline);
+            rpass.set_vertex_buffer(0, vertex_buffer.slice(..));
+            rpass.set_push_constants(
+                ShaderStages::FRAGMENT,
+                0,
+                bytemuck::bytes_of(&self.colorwheel),
+            );
+            rpass.draw(0..self.strokes.len() as u32, 0..1);
+        }
+        queue.submit(Some(encoder.finish()));
+        output_texture.present();
+    }
+
+    pub fn color_wheel_toogle(&mut self) {
+        self.colorwheel.toggle();
+    }
+
+    pub fn set_color(&mut self, color: [f32; 3]) {
+        self.colorwheel.set_color(color);
+    }
+
+    pub fn resize_window(&mut self, window_size: PhysicalSize<u32>) {
+        self.window_size = window_size
     }
 }
--- src/paint.rs
+++ src/paint.rs
@@ -4,12 +4,10 @@ use crate::colorwheel::ColorWheel;
 use rand::Rng;
 use std::borrow::Cow;
 use wgpu::{
-    util::{BufferInitDescriptor, DeviceExt},
-    Backends, BufferUsages, Color, CommandEncoderDescriptor, DeviceDescriptor, Features,
-    FragmentState, Instance, Limits, LoadOp, MultisampleState, Operations,
+    Backends, DeviceDescriptor, Features, FragmentState, Instance, Limits, MultisampleState,
     PipelineLayoutDescriptor, PowerPreference, PrimitiveState, PrimitiveTopology,
-    RenderPassColorAttachment, RenderPassDescriptor, RenderPipelineDescriptor,
-    RequestAdapterOptions, ShaderModuleDescriptor, ShaderSource, VertexState,
+    RenderPipelineDescriptor, RequestAdapterOptions, ShaderModuleDescriptor, ShaderSource,
+    VertexState,
 };
 use wgpu::{PushConstantRange, ShaderStages};
 
@@ -96,8 +94,6 @@ pub fn run(event_loop: EventLoop<()>, window: Window) {
 
     let mut brush = Brush::default();
     let mut canvas = Canvas::new(size);
-    let mut colorwheel = ColorWheel::default();
-    let mut strokes = vec![];
     let mut rng = rand::thread_rng();
 
     event_loop.run(move |event, _, control_flow| {
@@ -115,45 +111,7 @@ pub fn run(event_loop: EventLoop<()>, window: Window) {
                 canvas.resize_window(new_size);
             }
             Event::RedrawRequested(_) => {
-                let vertex_buffer = device.create_buffer_init(&BufferInitDescriptor {
-                    label: None,
-                    contents: bytemuck::cast_slice(&strokes),
-                    usage: BufferUsages::VERTEX,
-                });
-                let output_texture = surface
-                    .get_current_texture()
-                    .expect("failed to get texture for rendering");
-                let view = output_texture
-                    .texture
-                    .create_view(&wgpu::TextureViewDescriptor::default());
-                let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor {
-                    label: Some("paint encoder"),
-                });
-                {
-                    let mut rpass = encoder.begin_render_pass(&RenderPassDescriptor {
-                        label: None,
-                        color_attachments: &[RenderPassColorAttachment {
-                            view: &view,
-                            resolve_target: None,
-                            ops: Operations {
-                                load: LoadOp::Clear(Color::WHITE),
-                                store: true,
-                            },
-                        }],
-                        depth_stencil_attachment: None,
-                    });
-                    rpass.set_pipeline(&render_pipeline);
-                    rpass.set_vertex_buffer(0, vertex_buffer.slice(..));
-                    rpass.set_push_constants(
-                        ShaderStages::FRAGMENT,
-                        0,
-                        bytemuck::bytes_of(&colorwheel),
-                    );
-                    rpass.draw(0..strokes.len() as u32, 0..1);
-                }
-
-                queue.submit(Some(encoder.finish()));
-                output_texture.present();
+                canvas.redraw_canvas(&device, &surface, &render_pipeline, &queue);
             }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
@@ -168,7 +126,7 @@ pub fn run(event_loop: EventLoop<()>, window: Window) {
                         brush.dec_radius()
                     }
                     if input.key_pressed(VirtualKeyCode::Space) {
-                        colorwheel.toggle();
+                        canvas.color_wheel_toogle();
                         window.request_redraw();
                     }
                     if input.key_released(VirtualKeyCode::Escape) || input.quit() {
@@ -182,15 +140,14 @@ pub fn run(event_loop: EventLoop<()>, window: Window) {
                             rng.gen_range(0.0..1.0),
                         ];
                         brush.set_color(color);
-                        colorwheel.set_color(color);
+                        canvas.set_color(color);
                         window.request_redraw();
                     }
                     if let Some((x, y)) = input.mouse() {
                         if let Some((start, end)) =
                             brush.draw_stroke(input.mouse_held(0), canvas.get_canvas_pos([x, y]))
                         {
-                            strokes.push(start);
-                            strokes.push(end);
+                            canvas.add_stroke(start, end);
                             window.request_redraw();
                         }
                     }
