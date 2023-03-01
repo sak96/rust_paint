fedb9d571bb5de3fbe961069ac704abfc2cf985e
sak96 <26397224+sak96@users.noreply.github.com>
2023-03-01 19:50:31 +0530

    Add web feature

--- src/bin/web/main.rs
+++ src/bin/web/main.rs
@@ -1,2 +1,11 @@
+use yew::prelude::*;
+
 fn main() {
+    yew::Renderer::<App>::new().render();
+}
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <h1>{env!("CARGO_PKG_NAME")}</h1>
+    }
 }
